a046a3f99a825b2798992b25f9f8acce5257686f
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-11-10 21:51:22 -0300

    feat: Enhance course detail and lessons views with context data

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -6,8 +6,8 @@ def courses_list(request):
 
 
 def course_detail(request, pk):
-    return render(request, "courses/course_detail.html")
+    return render(request, "courses/course_detail.html", {"pk": pk})
 
 
 def course_lessons(request, pk):
-    return render(request, "courses/course_lessons.html")
+    return render(request, "courses/course_lessons.html", {"pk": pk})
