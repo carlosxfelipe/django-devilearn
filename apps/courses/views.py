7355f1cb9bed60022a59b89cba7ed00c36d32702
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-11-10 22:12:17 -0300

    feat: Implement dynamic course listing in courses view and template

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -2,7 +2,37 @@ from django.shortcuts import render
 
 
 def courses_list(request):
-    return render(request, "courses/courses.html")
+    courses = [
+        {
+            "id": 1,
+            "level": "Beginner",
+            "rating": 5.0,
+            "course_title": "Three-month Course to Learn the Basics of Python and Start Coding.",
+            "instructor": "Alison Walsh",
+        },
+        {
+            "id": 2,
+            "level": "Beginner",
+            "rating": 5.0,
+            "course_title": "Beginner's Guide to Successful Company Management: Business And More",
+            "instructor": "Patty Kutch",
+        },
+        {
+            "id": 3,
+            "level": "Beginner",
+            "rating": 5.0,
+            "course_title": "A Fascinating Theory of Probability. Practice. Application. How to Outplay...",
+            "instructor": "Alonzo Murray",
+        },
+        {
+            "id": 4,
+            "level": "Beginner",
+            "rating": 5.0,
+            "course_title": "Introduction: Machine Learning and LLM. Implementation in Modern Software",
+            "instructor": "Gregory Harris",
+        },
+    ]
+    return render(request, "courses/courses.html", {"courses": courses})
 
 
 def course_detail(request, pk):
